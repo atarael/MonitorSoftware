1fd03c6fa87a1797a076a6dfa0a2a9ba16280d28
unknown <Sara-Ayash>
2020-08-20 00:19:59 +0300

    new

---
+++ Client/Client/ClientForm.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+namespace Client
+{
+    public partial class ClientForm : Form
+    {
+        Socket ClientSocket;
+        ListBox chat;
+        public ClientForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            // convert msg to byte[]
+            ASCIIEncoding aEncoding = new ASCIIEncoding();
+            byte[] sendMsg = new byte[1500];
+            sendMsg = aEncoding.GetBytes(txbMsg.Text);
+            // send msg
+            ClientSocket.Send(sendMsg);
+            // add msg to list box
+            chat.Items.Add("ME: "+ txbMsg.Text);
+            txbMsg.Text = "";
+            
+
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            btnConnect.Enabled = false;
+            btnSendMsg.Enabled = true;
+            String Ipadress = txbIP.Text;
+            String clientName = txbClientName.Text;
+            
+            try
+            {
+
+                int port = 13000;
+                ClientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+              
+                IPEndPoint ep = new IPEndPoint(IPAddress.Parse(Ipadress), port);
+                ClientSocket.Connect(ep);
+                txbChat.Text = clientName + " connected!";
+                txbIP.Text = "sara";
+                //Console.WriteLine();
+                while (true)
+                {
+
+                    string messageFromClient = null;
+                    Console.WriteLine("Enter the message");
+                    messageFromClient = Console.ReadLine();
+                    ClientSocket.Send(System.Text.Encoding.ASCII.GetBytes(messageFromClient), 0, messageFromClient.Length, SocketFlags.None);
+                    byte[] MsgFromServer = new byte[1024];
+                    int size = ClientSocket.Receive(MsgFromServer);
+                    Console.WriteLine("Server: " + System.Text.Encoding.ASCII.GetString(MsgFromServer, 0, size));
+
+                }
+            }
+            catch (SocketException ex)
+            {
+                //ShowErrorDialog(ex.Message);
+            }
+            catch (ObjectDisposedException ex)
+            {
+               // ShowErrorDialog(ex.Message);
+            }
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,50 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 13000;
+            string IpAddress = "192.168.1.3";
+            Socket ServerListener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse(IpAddress), port);
+            ServerListener.Bind(ep);
+            ServerListener.Listen(100);
+            Console.WriteLine("Server is  Listening...");
+            Socket ClientSocket = default(Socket);
+            int counter = 0;
+            Program p = new Program();
+            while (true)
+            {
+                counter++;
+                ClientSocket = ServerListener.Accept();
+                Console.WriteLine(counter + " clients connected");
+                Thread UserThread = new Thread(new ThreadStart(() => p.User(ClientSocket)));
+                UserThread.Start();
+
+            }
+        }
+        public void User(Socket client)
+        {
+            while (true)
+            {
+                byte[] msg = new byte[1024];
+                int size = client.Receive(msg);
+                client.Send(msg, 0, size, SocketFlags.None);
+            }
+
+        }
+    }
+}
+
