54c9c3b4a417833e05a2588a78259f6f82697563
atarael <60358856+atarael@users.noreply.github.com>
2020-10-12 18:34:29 +0300

    try6

---
+++ ServerSide/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerSide
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new ServerForm());
+        }
+    }
+}
