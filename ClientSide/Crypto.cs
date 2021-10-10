290ac527dba7ddcac659cf0251351ff142c1e23f
atarael <60358856+atarael@users.noreply.github.com>
2021-10-10 21:12:37 +0300

    Update Crypto.cs

--- ClientSide/Crypto.cs
+++ ClientSide/Crypto.cs
@@ -1,147 +1,3 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using System.IO;
-//using System.Security.Cryptography;
-//using System.Windows.Forms;
-////using ControlzEx.Standard;
-////using Chilkat;
-
-//namespace ClientSide
-//{
-//    public class Crypto
-//    {
-
-//        public static RSACryptoServiceProvider csp = new RSACryptoServiceProvider(2048);
-
-//        //how to get the private key
-//        //  private  static System.Security.Cryptography.RSAParameters privKey = csp.ExportParameters(true);
-
-
-//        //and the public key ...
-
-
-//        public static string getPubKey()
-//        {
-//            System.Security.Cryptography.RSAParameters pubKey = csp.ExportParameters(false);
-
-//            //we need some buffer
-//            var sw = new System.IO.StringWriter();
-//            //we need a serializer
-//            var xs = new System.Xml.Serialization.XmlSerializer(typeof(RSAParameters));
-//            //serialize the key into the stream
-//            xs.Serialize(sw, pubKey);
-//            //get the string from the stream
-//            string pubKeyString = sw.ToString();
-//            return pubKeyString;
-//        }
-//        public static string Encryption(string strText)
-//        {
-//            var publicKey = "<RSAKeyValue><Modulus>21wEnTU+mcD2w0Lfo1Gv4rtcSWsQJQTNa6gio05AOkV/Er9w3Y13Ddo5wGtjJ19402S71HUeN0vbKILLJdRSES5MHSdJPSVrOqdrll/vLXxDxWs/U0UT1c8u6k/Ogx9hTtZxYwoeYqdhDblof3E75d9n2F0Zvf6iTb4cI7j6fMs=</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>";
-//            // string publicKey = getPubKey();
-//            var testData = Encoding.UTF8.GetBytes(strText);
-
-//            using (var rsa = new RSACryptoServiceProvider(1024))
-//            {
-//                try
-//                {
-//                    // client encrypting data with public key issued by server                    
-//                    rsa.FromXmlString(publicKey.ToString());
-
-//                    var encryptedData = rsa.Encrypt(testData, true);
-//                    var base64Encrypted = Convert.ToBase64String(encryptedData);
-//                    return base64Encrypted;
-//                }
-//                catch ( CryptographicException ex) {
-//                    ShowErrorDialog("CryptographicException:\n" + ex);
-//                    Console.WriteLine("CryptographicException:\n" + ex);
-//                    //return EncryptionByParts(strText );
-//                    return string.Empty;
-//                }
-
-//                finally
-//                {
-//                    rsa.PersistKeyInCsp = false;
-//                }
-//            }
-//        }
-//        private static void ShowErrorDialog(string message)
-//        {
-//            MessageBox.Show(message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
-//        }
-//        private static string EncryptionByParts(string strText )
-//        {
-//            int maxSize = 200;
-//            string subData = "";
-//            string sumEncrypt = "";
-
-//            if (strText.Length > maxSize)
-//            {
-
-//                for (int i = 0; i < strText.Length; i += maxSize)
-//                {
-//                    if ((i + maxSize) > strText.Length)
-//                    {
-//                        subData += strText.Substring(i, strText.Length);                      
-//                    }                           
-//                    else
-//                    {
-//                        subData += strText.Substring(i, i + maxSize);
-//                    }
-//                    sumEncrypt += Encryption(subData);
-//                }
-
-
-//            }
-//            return sumEncrypt;
-//        }
-
-//        public static string Decryption(string strText)
-//        {
-//            var privateKey = "<RSAKeyValue><Modulus>21wEnTU+mcD2w0Lfo1Gv4rtcSWsQJQTNa6gio05AOkV/Er9w3Y13Ddo5wGtjJ19402S71HUeN0vbKILLJdRSES5MHSdJPSVrOqdrll/vLXxDxWs/U0UT1c8u6k/Ogx9hTtZxYwoeYqdhDblof3E75d9n2F0Zvf6iTb4cI7j6fMs=</Modulus><Exponent>AQAB</Exponent><P>/aULPE6jd5IkwtWXmReyMUhmI/nfwfkQSyl7tsg2PKdpcxk4mpPZUdEQhHQLvE84w2DhTyYkPHCtq/mMKE3MHw==</P><Q>3WV46X9Arg2l9cxb67KVlNVXyCqc/w+LWt/tbhLJvV2xCF/0rWKPsBJ9MC6cquaqNPxWWEav8RAVbmmGrJt51Q==</Q><DP>8TuZFgBMpBoQcGUoS2goB4st6aVq1FcG0hVgHhUI0GMAfYFNPmbDV3cY2IBt8Oj/uYJYhyhlaj5YTqmGTYbATQ==</DP><DQ>FIoVbZQgrAUYIHWVEYi/187zFd7eMct/Yi7kGBImJStMATrluDAspGkStCWe4zwDDmdam1XzfKnBUzz3AYxrAQ==</DQ><InverseQ>QPU3Tmt8nznSgYZ+5jUo9E0SfjiTu435ihANiHqqjasaUNvOHKumqzuBZ8NRtkUhS6dsOEb8A2ODvy7KswUxyA==</InverseQ><D>cgoRoAUpSVfHMdYXW9nA3dfX75dIamZnwPtFHq80ttagbIe4ToYYCcyUz5NElhiNQSESgS5uCgNWqWXt5PnPu4XmCXx6utco1UVH8HGLahzbAnSy6Cj3iUIQ7Gj+9gQ7PkC434HTtHazmxVgIR5l56ZjoQ8yGNCPZnsdYEmhJWk=</D></RSAKeyValue>";
-//            //string privateKey = getPriKey();
-//            var testData = Encoding.UTF8.GetBytes(strText);
-
-//            using (var rsa = new RSACryptoServiceProvider(1024))
-//            {
-//                try
-//                {
-//                    var base64Encrypted = strText;
-
-//                    // server decrypting data with private key                    
-//                    rsa.FromXmlString(privateKey);
-
-//                    var resultBytes = Convert.FromBase64String(base64Encrypted);
-//                    var decryptedBytes = rsa.Decrypt(resultBytes, true);
-//                    var decryptedData = Encoding.UTF8.GetString(decryptedBytes);
-//                    return decryptedData.ToString();
-//                }
-//                finally
-//                {
-//                    rsa.PersistKeyInCsp = false;
-//                }
-//            }
-//        }
-
-//        private static string getPriKey()
-//        {
-//            System.Security.Cryptography.RSAParameters privKey = csp.ExportParameters(true);
-//            //we need some buffer
-//            var sw = new System.IO.StringWriter();
-//            //we need a serializer
-//            var xs = new System.Xml.Serialization.XmlSerializer(typeof(RSAParameters));
-//            //serialize the key into the stream
-//            xs.Serialize(sw, privKey);
-//            //get the string from the stream
-//            string privKeyString = sw.ToString();
-//            return privKeyString;
-//        }
-//    }
-//}
-
-
 using System;
 using System.IO;
 using System.Security.Cryptography;
@@ -238,10 +94,15 @@ namespace ClientSide
 
         public static string Encrypt(string clearText)
         {
+            // IMPORTENT! Key encryption is required in the configuration file 
+            // but the issue of encryption is not within the scope of our project 
+            // so we did not delay it 
+            
             string EncryptionKey = "MAKV2SPBNI99212";
             byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
             using (Aes encryptor = Aes.Create())
             {
+              
                 Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
                 encryptor.Key = pdb.GetBytes(32);
                 encryptor.IV = pdb.GetBytes(16);
@@ -260,6 +121,10 @@ namespace ClientSide
 
         public static string Decrypt(string cipherText)
         {
+            // IMPORTENT! Key encryption is required in the configuration file 
+            // but the issue of encryption is not within the scope of our project 
+            // so we did not delay it 
+            
             string EncryptionKey = "MAKV2SPBNI99212";
             byte[] cipherBytes = Convert.FromBase64String(cipherText);
             using (Aes encryptor = Aes.Create())
